43efcceccedb47003427d21882c0e04803a77329
jobin212 <jobin212@gmail.com>
2022-09-20 13:42:08 -0700

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/jobin212/rc-logger
+
+go 1.18
---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+/*
+Implement a buffered logger that can be used by multiple callers concurrently to log messages.
+
+The logger will buffer logged messages in memory and flush out buffered content when the size reaches a configured threshold
+or the time has passed a configured threshold since the first buffered message was logged. You can assume the flush function
+is already provided, or use print to screen as implementation. The flush operation should not block the log method.
+
+*/
+
+func main() {
+
+}
