6460a5f6ffe375d4d166e5d922420d4d8cd89679
jobin212 <jobin212@gmail.com>
2022-09-20 13:59:51 -0700

    flush when full

--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
 /*
 Implement a buffered logger that can be used by multiple callers concurrently to log messages.
 
@@ -9,6 +15,50 @@ is already provided, or use print to screen as implementation. The flush operati
 
 */
 
-func main() {
+var capacity int = 10
+var timeThreshold time.Duration = 5 * time.Second
 
+type Logger struct {
+	buffer chan string
+}
+
+func (l *Logger) log(msg string) {
+	select {
+	case l.buffer <- msg:
+	default:
+		go l.flush()
+		l.buffer <- msg
+	}
+}
+
+func (l *Logger) flush() {
+	for {
+		select {
+		case msgToPrint := <-l.buffer:
+			fmt.Println(msgToPrint)
+		default:
+			return
+		}
+	}
+}
+
+var wg sync.WaitGroup
+
+func main() {
+	logger := &Logger{
+		buffer: make(chan string, capacity),
+	}
+
+	for worker := 0; worker < 5; worker++ {
+		wg.Add(1)
+		go func(workerId int) {
+			defer wg.Done()
+			for i := 0; i < 10; i++ {
+				logger.log(fmt.Sprintf("Worker: %d Msg %d", workerId, i))
+			}
+		}(worker)
+	}
+
+	wg.Wait()
+	logger.flush()
 }
